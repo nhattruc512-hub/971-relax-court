a35b91b2f3268a2490e5db23d1a54e2663506b19
nhattruc512-hub <nhattruc512@gmail.com>
2026-08-19 18:40:17 +0700

    Add Supabase API configuration

---
+++ config.js
@@ -0,0 +1,4 @@
+window.R971 = {
+  API_URL: "https://dinqlgaveujdeyisgpty.supabase.co/functions/v1/r971-api",
+  ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRpbnFsZ2F2ZXVqZGV5aXNncHR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcxMjQxNzAsImV4cCI6MjEwMjcwMDE3MH0.L5aitJLmaGC4yopIzjwkQomwQ0H9dSOfNWqvAgwrzQI"
+};
